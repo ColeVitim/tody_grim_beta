2604506da8de4d07cc51292ccd06650807e91635
Victor Nogueira <victor.ca.nogueira@gmail.com>
2023-07-09 22:39:25 -0300

    correcao de luz

--- Assets/Scripts/GlobalIluminacao.cs
+++ Assets/Scripts/GlobalIluminacao.cs
@@ -13,7 +13,7 @@ public class GlobalIluminacao : MonoBehaviour
     void Start()
     {
         light = GetComponent<Light2D>();
-        light.intensity = 0.04f;
+        light.intensity = 0.15f;
     }
 
     // Update is called once per frame
